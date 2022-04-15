6848eddd0113909276fe99651c960368381b009b
strive-up <striveup@inbox.ru>
2022-04-15 14:10:01 +0300

    df
    ds

--- ClientoTerapia/js/main.js
+++ ClientoTerapia/js/main.js
@@ -11,6 +11,12 @@
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_menu_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/menu.js */ "./src/js/components/menu.js");
 /* harmony import */ var _components_menu_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_components_menu_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_services_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/services.js */ "./src/js/components/services.js");
+/* harmony import */ var _components_services_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_components_services_js__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components_svg_fill_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/svg-fill.js */ "./src/js/components/svg-fill.js");
+/* harmony import */ var _components_svg_fill_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_components_svg_fill_js__WEBPACK_IMPORTED_MODULE_2__);
+
+
 
 
 /***/ }),
@@ -25,6 +31,87 @@ var menuBtn = document.querySelector('.header__burger');
 var menu = document.querySelector('.header__nav');
 menuBtn.addEventListener('click', function () {
   menu.classList.toggle('menu__active');
+});
+
+/***/ }),
+
+/***/ "./src/js/components/services.js":
+/*!***************************************!*\
+  !*** ./src/js/components/services.js ***!
+  \***************************************/
+/***/ (() => {
+
+$(document).ready(function () {
+  if (window.innerWidth >= 991) {
+    //When page loads...
+    $(".tab_content, .tab_content_child").hide(); //Hide all content
+
+    $("ul.tabs li:first").addClass("active").show(); //Activate first tab
+
+    $("ul.tabs_child li:first").addClass("active").show(); //Activate first tab
+
+    $(".tab_content:first").show(); //Show first tab content
+
+    $(".tab_content_child:first").show(); //Show first tab content
+    //On Click Event
+
+    $("ul.tabs li").click(function () {
+      $("ul.tabs li").removeClass("active"); //Remove any "active" class
+
+      $(this).addClass("active"); //Add "active" class to selected tab
+
+      $(".tab_content").hide(); //Hide all tab content
+
+      var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+
+      $(activeTab).fadeIn(); //Fade in the active ID content
+
+      return false;
+    }); //On Click child tabs Event
+
+    $("ul.tabs_child li").click(function () {
+      $("ul.tabs_child li").removeClass("active"); //Remove any "active" class
+
+      $(this).addClass("active"); //Add "active" class to selected tab
+
+      $(".tab_content_child").hide(); //Hide all tab content 
+
+      var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+
+      $(activeTab).fadeIn(); //Fade in the active ID content 
+
+      return false;
+    });
+  }
+});
+
+/***/ }),
+
+/***/ "./src/js/components/svg-fill.js":
+/*!***************************************!*\
+  !*** ./src/js/components/svg-fill.js ***!
+  \***************************************/
+/***/ (() => {
+
+$('img.img-svg').each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function (data) {
+    var $svg = $(data).find('svg');
+
+    if (typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass + ' replaced-svg');
+    }
+
+    $svg = $svg.removeAttr('xmlns:a');
+
+    if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'));
+    }
+
+    $img.replaceWith($svg);
+  }, 'xml');
 });
 
 /***/ })
