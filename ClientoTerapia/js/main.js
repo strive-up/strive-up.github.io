54e0fbdb4f57d777c1e8d6ea8047235141d4422f
strive-up <69415211+strive-up@users.noreply.github.com>
2022-06-03 18:31:20 +0300

    f
    fdg

--- ClientoTerapia/js/main.js
+++ ClientoTerapia/js/main.js
@@ -288,21 +288,32 @@ $('.quiz').each(function () {
   });
 });
 $('.btn__quiz').click(function () {
-  var quizContent = $(this).closest('.quiz__page__content.active');
-  var quizToggler = $(this).closest('.guiz__page').prev().find('li.active');
-
   if ($(this).hasClass('btn__quiz--prev')) {
-    if (quizToggler.prev().length) {
-      quizToggler.prev().addClass('active').siblings().removeClass('active');
-      quizContent.prev().show().addClass('active').siblings().hide().removeClass('active');
+    if ($('.quiz__pagination .quiz__pag__item.active').prev().length) {
+      $('.quiz__pagination .quiz__pag__item.active').prev().addClass('active').siblings().removeClass('active');
+      $('.quiz__page__content.active').prev().show().addClass('active').siblings().hide().removeClass('active');
     }
   } else if ($(this).hasClass('btn__quiz--next')) {
-    if (quizToggler.next().length) {
-      quizToggler.next().addClass('active').siblings().removeClass('active');
-      quizContent.next().show().addClass('active').siblings().hide().removeClass('active');
+    if ($('.quiz__pagination .quiz__pag__item.active').next().length) {
+      $('.quiz__pagination .quiz__pag__item.active').next().addClass('active').siblings().removeClass('active');
+      $('.quiz__page__content.active').next().show().addClass('active').siblings().hide().removeClass('active');
     }
   }
 });
+var acc = document.getElementsByClassName("quiz__switch__item"),
+    i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function () {
+    for (j = 0; j < acc.length; j++) {
+      if (acc[j] !== this) {
+        acc[j].classList.remove("active");
+      } else {
+        this.classList.toggle("active");
+      }
+    }
+  };
+}
 
 /***/ }),
 
