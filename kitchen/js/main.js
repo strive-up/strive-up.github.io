b8685655071518eff72a052425d31acda8480c53
strive-up <69415211+strive-up@users.noreply.github.com>
2022-10-09 17:46:48 +0300

    вавыа
    выаыва

---
+++ kitchen/js/main.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/* import './_vendor';
+import vars from './_vars';
+import './_functions';
+import './_components';
+ */
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
