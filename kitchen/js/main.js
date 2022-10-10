ec8187b9a257c0be23666e03995325a00e8474ef
strive-up <69415211+strive-up@users.noreply.github.com>
2022-10-10 11:52:43 +0300

    vfg
    fdg

--- kitchen/js/main.js
+++ kitchen/js/main.js
@@ -3,11 +3,28 @@ var __webpack_exports__ = {};
 /*!************************!*\
   !*** ./src/js/main.js ***!
   \************************/
-/* import './_vendor';
-import vars from './_vars';
-import './_functions';
-import './_components';
- */
+const menuBtn = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__menu');
+
+const hideMenu = event => {
+  $("body").css("overflow", "auto");
+  menu.classList.remove('menu__active');
+};
+
+const close = event => !menu.contains(event.target) && hideMenu(event);
+
+menuBtn.addEventListener('click', event => {
+  event.stopPropagation();
+
+  if (!menu.classList.contains('menu__active')) {
+    $("body").css("overflow", "hidden");
+    menu.classList.add('menu__active');
+  } else {
+    $("body").css("overflow", "auto");
+    menu.classList.remove('menu__active');
+  }
+});
+window.addEventListener('click', close);
 /******/ })()
 ;
 //# sourceMappingURL=main.js.map
