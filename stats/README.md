a7b7fbfbbbac8e0c5aea9934b78d7913c5cb03d3
strive-up <69415211+strive-up@users.noreply.github.com>
2023-06-11 14:31:27 +0300

    sd
    sd

---
+++ stats/README.md
@@ -0,0 +1,9 @@
+![preview](https://img.ge/images/51490382529631817613.png)
+# faceitEloWidget
+This is a visual widget for streamers who grind faceit elo. It displays skill group(faceit level with usual graphic) and elo in front of a stadium on a transparent background.
+widget updates every 20 seconds
+
+# how to set it up:
+["https://gogorikidze.github.io/faceitEloWidget/?user=" + YoutFaceitUsername] is your url.\
+put the url^ as a browser source in your streaming software. width: 85px | height: 35px.\
+you are good to go.
---
+++ stats/elo.js
@@ -0,0 +1,22 @@
+const url = new URLSearchParams(window.location.search);
+const nickname = url.get('user');
+
+var elodisplay = document.getElementById('elo');
+var levelpic = document.getElementById('levelpic');
+
+updateelo();
+setInterval(updateelo, 20000);
+function updateelo(){
+  var oReq = new XMLHttpRequest();
+  oReq.open('GET', 'https://open.faceit.com/data/v4/players?nickname=' + nickname + '&game=csgo&apiKey=41ee16c9-7c6e-4605-86e2-91886d85ac53');
+  oReq.setRequestHeader('accept', 'application/json');
+  oReq.setRequestHeader('Authorization', 'Bearer 41ee16c9-7c6e-4605-86e2-91886d85ac53');
+  oReq.send();
+  oReq.onload = function() {
+    elo = JSON.parse(oReq.responseText).games.csgo.faceit_elo;
+    matches = JSON.parse(oReq.responseText).games.csgo.faceit_matches;
+    levelpic.src = "https://cdn-frontend.faceit.com/web/960/src/app/assets/images-compress/skill-icons/skill_level_"+ JSON.parse(oReq.responseText).games.csgo.skill_level + "_svg.svg";
+    elodisplay.innerText = elo;
+    elodisplay.innerText = matches;
+  }
+}
